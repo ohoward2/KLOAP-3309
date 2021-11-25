a509d4ec6c84b186c6e4bd54d8783413c95a4911
pmoeini1 <pmoeini@uwo.ca>
2021-11-24 19:10:45 -0500

    Data modification

---
+++ assignment3/sql/ex6.sql
@@ -0,0 +1,5 @@
+DELETE FROM Athlete WHERE age>22;
+
+UPDATE Team SET country='United States' WHERE ranking<5 AND country='Canada';
+
+UPDATE Athlete SET aPosition='Center' WHERE height>213 AND weight>125;
